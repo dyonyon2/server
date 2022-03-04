b2b3d9301afcf6a653601da8084ad31117097950
jychoi9712 <jychoi9712@naver.com>
2022-03-04 14:49:59 +0900

    호스트 추가함

--- server/settings.py
+++ server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pufc(%1g53*b_m7m^s@t+ew=70ih-yfxi5_oe#&yrvao_k)s41
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
